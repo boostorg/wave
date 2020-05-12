2970a40ac4dd83e0350bbe738366a4e2cb1849f7
Andrey Semashev <Lastique@users.noreply.github.com>
2020-05-12 18:28:52 +0300

    Remove use of deprecated header boost/detail/iterator.hpp. (#91)
    Remove deprecated boost header and use std::iterator_traits instead

--- samples/token_statistics/xlex/xpressive_lexer.hpp
+++ samples/token_statistics/xlex/xpressive_lexer.hpp
@@ -16,9 +16,9 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <iterator>
 #include <algorithm>
 
-#include <boost/detail/iterator.hpp>
 #include <boost/xpressive/xpressive.hpp>
 
 namespace boost {
@@ -36,7 +36,7 @@ template <
 class xpressive_lexer
 {
 private:
-    typedef typename boost::iterators::iterator_value<Iterator>::type
+    typedef typename std::iterator_traits<Iterator>::value_type
         char_type;
     typedef std::basic_string<char_type> string_type;
     
