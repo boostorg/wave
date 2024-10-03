122d4a4b1f0a11ca4e15f5b257e6f48d41d67f77
Jeff Trull <edaskel@att.net>
2024-10-02 21:05:38 -0700

    Add additional test for initial empty line in included file

--- test/testwave/testfiles/t_5_040.cpp
+++ test/testwave/testfiles/t_5_040.cpp
@@ -12,6 +12,7 @@
 #include "t_5_040_001.hpp" // #if as first line should emit line directive
 #include "t_5_040_002.hpp" // #define as first line should emit line directive
 #include "t_5_040_003.hpp" // ensure nested includes emit line directive
+#include "t_5_040_005.hpp" // ensure empty initial lines produce line directive
 
 t_5_040_a
 #if 1
@@ -30,7 +31,9 @@ t_5_040_b
 //R t_5_040_004
 //R #line 2 "t_5_040_003.hpp"
 //R t_5_040_003
-//R #line 16 "t_5_040.cpp"
+//R #line 15 "t_5_040_005.hpp"
+//R t_5_040_005
+//R #line 17 "t_5_040.cpp"
 //R t_5_040_a
 //R 
 //R t_5_040_b
@@ -62,6 +65,10 @@ t_5_040_b
 //H 10: t_5_040_004.hpp(3): #endif
 //H 06: 
 //H 06: 
-//H 10: t_5_040.cpp(17): #if
-//H 11: t_5_040.cpp(17): #if 1: 1
-//H 10: t_5_040.cpp(19): #endif
+//H 10: t_5_040.cpp(15): #include "t_5_040_005.hpp"
+//H 04: "t_5_040_005.hpp"
+//H 05: t_5_040_005.hpp ($B(t_5_040_005.hpp))
+//H 06: 
+//H 10: t_5_040.cpp(18): #if
+//H 11: t_5_040.cpp(18): #if 1: 1
+//H 10: t_5_040.cpp(20): #endif
