3e758814c73c1bebdf039bb10a21fad51c662e35
Jeff Trull <edaskel@att.net>
2024-10-02 20:41:29 -0700

    Correct copyright info

--- test/testwave/testfiles/t_5_040_003.hpp
+++ test/testwave/testfiles/t_5_040_003.hpp
@@ -9,7 +9,7 @@ t_5_040_003
     Boost.Wave: A Standard compliant C++ preprocessor library
     http://www.boost.org/
 
-    Copyright (c) 2001-2012 Hartmut Kaiser. Distributed under the Boost
+    Copyright (c) 2024 Nick Nobles. Distributed under the Boost
     Software License, Version 1.0. (See accompanying file
     LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
